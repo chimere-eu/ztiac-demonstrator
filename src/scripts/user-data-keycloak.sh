b971a868f9b0b36168c1f26ccf653fcca7952656
Guillaume-Alexandre Chaizy <guillaume-alexandre.chaizy@chimere.eu>
2025-09-10 17:09:29 +0200

    chore: harden default Keycloak admin credentials
    Updates the default Keycloak admin username and password to be less predictable.
    
    This change encourages users to change the default credentials upon initial setup, improving security.

--- src/scripts/user-data-keycloak.sh
+++ src/scripts/user-data-keycloak.sh
@@ -9,6 +9,6 @@ sudo usermod -aG docker outscale
 cd /opt/
 mkdir keycloak
 cd keycloak
-docker run -p 8080:8080 -e KC_BOOTSTRAP_ADMIN_USERNAME=admin -e KC_BOOTSTRAP_ADMIN_PASSWORD=admin quay.io/keycloak/keycloak:26.3.3 start-dev
+docker run -p 8080:8080 -e KC_BOOTSTRAP_ADMIN_USERNAME=UsernameToChange -e KC_BOOTSTRAP_ADMIN_PASSWORD=PasswordToChange quay.io/keycloak/keycloak:26.3.3 start-dev
 # For production, see https://www.keycloak.org/getting-started/getting-started-docker
 
